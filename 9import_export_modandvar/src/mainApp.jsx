ed84e61785d6b357ad9310cfcf9b193ec1e5d759
Munsif <munsifalimisri69@gmail.com>
2021-01-05 07:59:41 +0500

    lecture 9 import export component,function and var

--- 9import_export_modandvar/src/mainApp.jsx
+++ 9import_export_modandvar/src/mainApp.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { topic, ExpList, ExpDefination, SumDefination, sum, ImportExp, theEnd } from './impExp';//the warning is bcoz i am not using topic anywhere insted i am using dot operator to use it 
+import { /*topic ,*/ ExpList, ExpDefination, SumDefination, sum, ImportExp, theEnd } from './impExp';//the warning is bcoz i am not using topic anywhere insted i am using dot operator to use it 
 import * as importExport from "./impExp";//if you doont want to wrtie upper import statmetent
 function mainApp() {
     return (
