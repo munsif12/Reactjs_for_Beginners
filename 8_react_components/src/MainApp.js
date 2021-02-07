f6b9a8bc15d1db93bac29769000aaa8bd06be9eb
Munsif <munsifalimisri69@gmail.com>
2021-02-07 23:07:57 +0500

    lecture 8 -> react components 20

---
+++ 8_react_components/src/MainApp.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Topic from './Topic'
+import Body from './Body'
+function MainApp() {
+    return (
+        <>
+            <Topic />
+            <Body />
+        </>
+    );
+}
+export default MainApp;
