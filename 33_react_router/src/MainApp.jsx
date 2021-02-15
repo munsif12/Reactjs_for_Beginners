9fec2285329f30190849f4c18ea6e86af45f0fff
Munsif <munsifalimisri69@gmail.com>
2021-02-16 00:45:40 +0500

    lecture 33 -> React roter :- to create a routing for website

--- 33_react_router/src/MainApp.jsx
+++ 33_react_router/src/MainApp.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Contect from "./Contect";
 import TopicDetils from "./TopicDetils";
 function MainApp() {
+  useEffect(() => {
+    alert("change the url to /Contect to see changes");
+  }, []);
   return (
     <div className="mainApp">
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/Contect" component={Contect} />
+        <Route exact path="/Contect" component={Contect} />
       </Switch>
       <TopicDetils />
     </div>
--- 33_react_router/src/TopicDetils.jsx
+++ 33_react_router/src/TopicDetils.jsx
@@ -6,50 +6,73 @@ function TopicDetils() {
       <h3>REACT-ROUTER-DOM DETAILS</h3>
       <ul>
         <li>
-          <p>
-            Basically React-router is use to render pages on the client side
-            what we were before was rendering pages on server side{" "}
-          </p>
+          Basically React-router is use to render pages on the client side what
+          we were before was rendering pages on server side.
         </li>
         <li>
-          <p>
-            React. js is an open-source JavaScript library that is used for
-            building user interfaces specifically for single-page applications.
-            ... React allows developers to create large web applications that
-            can change data, without reloading the page. The main purpose of
-            React is to be fast, scalable, and simple.
-          </p>
+          React. js is an open-source JavaScript library that is used for
+          building user interfaces specifically for single-page applications.
+          ... React allows developers to create large web applications that can
+          change data, without reloading the page. The main purpose of React is
+          to be fast, scalable, and simple.
         </li>
         <li>
-          <h6>Example:</h6>
+          <p className="heading">Example:</p>
           <ul>
             <li>
-              <p>
-                import {" { BrowserRouter, Route, Link }"} from
-                "react-router-dom";
-                <ul>
-                  <li>
-                    <p>
-                      BrowserRouter :- jisn compontents ke ham na render krwana
-                      ho unko BrowserRouter ka ander likhty ha like hamy pure
-                      website route krwani haa to pure mainApp iska ander likhy
-                      ga
-                      <ul>
-                        <li>
-                          {" <BrowserRouter> "}
-                          {"<MainApp />"}
-                          {" </BrowserRouter>"}
-                        </li>
-                      </ul>
-                    </p>
-                  </li>
-                </ul>
-              </p>
+              import {" { BrowserRouter, Route, Link }"} from
+              "react-router-dom";
+              <ol>
+                <li>
+                  <p>
+                    <span className="routerComponents">BrowserRouter</span> :-
+                    jisn compontents ke ham na render krwana ho unko
+                    BrowserRouter ka ander likhty ha like hamy pure website
+                    route krwani haa to pure mainApp iska ander likhy ga
+                    <ul>
+                      <li>
+                        {" <BrowserRouter> "} <br />
+                        {"<MainApp />"} <br />
+                        {" </BrowserRouter>"}
+                      </li>
+                    </ul>
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    <span className="routerComponents">Swith:- </span> iska use
+                    ham given path ke traf lajany ka leya krty h iska ander
+                    jitny mazee paths bana sakty ho ap{" "}
+                  </p>
+                  <p>
+                    It just means that whenever a route's path matches the url
+                    path, the router will render the route's component.
+                    <ul>
+                      <li>
+                        {"<Switch>"} <br />
+                        {"<Route exact path='/' component={Home} /> "} <br />
+                        {'<Route path="/Contect" component={Contect} />'}
+                        <br />
+                        {"</Switch>"}
+                      </li>
+                    </ul>
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    <span className="routerComponents">Path:- </span> is
+                    component ka use ka ap konsa path bana chahty ha is
+                    component ke 3 propertires hote h
+                    <ul>
+                      <li>Exect</li>
+                      <li>Path="/"</li>
+                      <li>Component={"{Ceomponentname}"}</li>
+                    </ul>
+                  </p>
+                </li>
+              </ol>
             </li>
           </ul>
-        </li>
-        <li>
-          <p></p>
         </li>
       </ul>
     </div>
