a5ab03e5b981deed550091aedab1a7d556d71dc0
Munsif <munsifalimisri69@gmail.com>
2021-02-20 23:51:11 +0500

    lecture 35 -> useParams in Reactjs

--- 35_useparams_hooks_in_reactjs/src/MainPage.jsx
+++ 35_useparams_hooks_in_reactjs/src/MainPage.jsx
@@ -12,8 +12,27 @@ function MainPage() {
       <div className="topicDetails">
         <h3>UseParams in Reactjs</h3>
         <ul>
+          <li>
+            useParams returns an object of key/value pairs of URL parameters.
+            Use it to access match.
+          </li>
           <li>useparams are use get value of the url of the dynamic contect</li>
           <li>like i created a page user/etcUserName change the url to see</li>
+          <li>
+            Example:
+            <ul>
+              <li>
+                {"<Route exact "}
+                <span>path="/user"</span>{" "}
+                {'component={() => <User pageName="User" />} /> '}
+              </li>
+              <li>
+                {"<Route exact "}
+                <span>path="/user/:userName"</span>{" "}
+                {'component={() => <User pageName="User" />} /> '}
+              </li>
+            </ul>
+          </li>
         </ul>
       </div>
       <div className="body">
