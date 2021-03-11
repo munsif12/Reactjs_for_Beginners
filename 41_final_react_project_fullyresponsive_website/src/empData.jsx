15d9b5f0d19a05dec31d90199cd4f0da83d079d5
Munsif <munsifalimisri69@gmail.com>
2021-03-11 23:27:15 +0500

    added employee data which is arrayof object like API

---
+++ 41_final_react_project_fullyresponsive_website/src/empData.jsx
@@ -0,0 +1,33 @@
+const empData = [
+  {
+    empName: "Munsif",
+    jobTitle: "Tech Lead",
+    university: "NUML",
+  },
+  {
+    empName: "Kazmii",
+    jobTitle: "Front End Engineer",
+    university: "NUML",
+  },
+  {
+    empName: "Hamza",
+    jobTitle: "Product Manager",
+    university: "NUML",
+  },
+  {
+    empName: "Arslan",
+    jobTitle: "Netwrok Analyst",
+    university: "NUML",
+  },
+  {
+    empName: "Abdul Wahab",
+    jobTitle: "BackEnd Developer",
+    university: "NUML",
+  },
+  {
+    empName: "Usman Shams",
+    jobTitle: "Android Developer",
+    university: "NUML",
+  },
+];
+export default empData;
