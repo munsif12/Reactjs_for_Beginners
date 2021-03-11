a2d95e58eb0d45bdb735824ea5b0b765c79542d2
Munsif <munsifalimisri69@gmail.com>
2021-03-11 23:29:25 +0500

    added Our TEAM Page in the router but no specified a button for the => tomorrow IA

---
+++ 41_final_react_project_fullyresponsive_website/src/Team.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import JokerImg from "./empImages/2106477.jpg";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import FacebookIcon from "@material-ui/icons/Facebook";
+function Team({ empName, jobTitle, university }) {
+  return (
+    <div class="card">
+      <div className="imgCont" back>
+        <img src={JokerImg} alt="John" />
+      </div>
+      <div className="empDetails">
+        <div className="empName">
+          <h1>{empName}</h1>
+        </div>
+        <div className="jobTitle">
+          <p class="title">{jobTitle}</p>
+        </div>
+        <div className="university">
+          <p>{university}</p>
+        </div>
+      </div>
+      <div className="empSocialLinks">
+        <NavLink to="#">
+          <FacebookIcon />
+        </NavLink>
+        <NavLink to="#">
+          <LinkedInIcon />
+        </NavLink>
+        <NavLink to="#">
+          <GitHubIcon />
+        </NavLink>
+      </div>
+      <div className="btnDiv">
+        <button>Contact</button>
+      </div>
+    </div>
+  );
+}
+
+export default Team;
