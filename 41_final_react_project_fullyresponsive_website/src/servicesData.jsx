9e4d6171bd4e9d652c6d5f46f198450da4b96174
Munsif <munsifalimisri69@gmail.com>
2021-03-13 23:38:10 +0500

    arrayOfObjects for dervices Page Card

---
+++ 41_final_react_project_fullyresponsive_website/src/servicesData.jsx
@@ -0,0 +1,32 @@
+const servicesData = [
+  {
+    sName: "Web Development",
+    sImg: "",
+  },
+  {
+    sName: "Android Development",
+    sImg: "",
+  },
+  {
+    sName: "Networking",
+    sImg: "",
+  },
+  {
+    sName: "CCTV",
+    sImg: "",
+  },
+  {
+    sName: "Software Development",
+    sImg: "",
+  },
+  {
+    sName: "Software Maintainance",
+    sImg: "",
+  },
+  {
+    sName: "Software Maintainance",
+    sImg: "",
+  },
+];
+
+export default servicesData;
