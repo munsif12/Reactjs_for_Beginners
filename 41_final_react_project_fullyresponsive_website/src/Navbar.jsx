88a678d3d3748219825649e46842f96ab6239110
Munsif <munsifalimisri69@gmail.com>
2021-03-13 23:30:57 +0500

    Adding a Hamburger to NavBar

--- 41_final_react_project_fullyresponsive_website/src/Navbar.jsx
+++ 41_final_react_project_fullyresponsive_website/src/Navbar.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 function Navbar() {
+  const [Display, setDisplay] = useState(false);
+  useEffect(() => {
+    console.log("running Efffect")
+    const currentDisplayProp = document.getElementById("rightNavElements").style.display;
+    console.log(currentDisplayProp)
+  });
+  function toggleNavbar() {
+    const toggleBtn = document.getElementById("hamburger");
+    toggleBtn.addEventListener("click", () => {
+      if (!Display) {
+        console.log(Display)
+        document.getElementById("rightNavElements").style.display = "block";
+        setDisplay(true);
+      }
+      else {
+        document.getElementById("rightNavElements").style.display = "none";
+        setDisplay(false);
+      }
+
+    })
+  }
   return (
     <div className="navbar">
       <div className="leftLogo">
         <h1>Munsif Ali Misri</h1>
       </div>
-      <div className="rightNavElements">
+      <NavLink to="#" className="toggleButton" onClick={toggleNavbar} id="hamburger">
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </NavLink>
+      <div className="rightNavElements" id="rightNavElements">
         <ul>
           <li className="list1">
             <NavLink
