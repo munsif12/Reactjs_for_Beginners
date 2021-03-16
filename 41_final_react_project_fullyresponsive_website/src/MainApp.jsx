ad8ac198cef4b1204642993decdc181cfa909f19
Munsif <munsifalimisri69@gmail.com>
2021-03-16 15:03:25 +0500

    main file of project

--- 41_final_react_project_fullyresponsive_website/src/MainApp.jsx
+++ 41_final_react_project_fullyresponsive_website/src/MainApp.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import Page from "./Page";
+import Contect from "./Contect";
+import Services from "./Services";
+import Footer from "./Footer";
+import Team from "./Team";
+import Errorpage from "./ErrorPage";
 import SvgHome from "./SvgHome";
 import SvgAbout from "./SvgAbout";
-import Contect from "./Contect";
-import Footer from "./Footer";
+import EmpData from "./empData";
 function MainApp() {
+  console.log(EmpData);
   return (
     <div className="mainApp">
       <Navbar />
       <Switch>
+        {/* HOME PAGE */}
         <Route
           exact
           path="/"
@@ -19,7 +25,7 @@ function MainApp() {
               <Page
                 pageName="Home"
                 moto="Grow Your Business With Us"
-                companyName="Developers Hub"
+                companyName="Company Name"
                 about="We are a team of Talent And Experianced Software Developers"
                 buttenInnerText="Get Stareted"
                 SvgHome={() => {
@@ -29,6 +35,7 @@ function MainApp() {
             );
           }}
         />
+        {/* ABOUT PAGE */}
         <Route
           exact
           path="/about"
@@ -47,11 +54,50 @@ function MainApp() {
             );
           }}
         />
+        {/* CONTECT US */}
         <Route
           exact
           path="/contect"
           component={() => {
             return <Contect />;
+          }}
+        />
+        PROJECTS
+        <Route
+          exact
+          path="/services"
+          component={() => {
+            return <Services />;
+          }}
+        />
+        {/*OUR TEAM */}
+        <Route
+          exact
+          path="/team"
+          component={() => {
+            return (
+              <div className="team">
+                <h1 className="teamMembers">TEAM MEMBERS</h1>
+                <div className="cardsWrapper">
+                  {EmpData.map((val) => {
+                    return (
+                      <Team
+                        empName={val.empName}
+                        jobTitle={val.jobTitle}
+                        empImage={val.empImage}
+                        university={val.university}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          }}
+        />
+        {/*ERROR PAGE */}
+        <Route
+          component={() => {
+            return <Errorpage />;
           }}
         />
       </Switch>
