301def7703abcf43662155059a4d351b90fa07c2
Munsif <munsifalimisri69@gmail.com>
2021-03-16 15:04:51 +0500

    contains arrayOfObject for emp name, img profession etc

--- 41_final_react_project_fullyresponsive_website/src/empData.jsx
+++ 41_final_react_project_fullyresponsive_website/src/empData.jsx
@@ -1,33 +1,53 @@
+import Munsif from "./teamEmpImages/Munsif.jpeg"
+import Hassan from "./teamEmpImages/hassanAliShahKazmii.jpeg"
+import Hamza from "./teamEmpImages/Hamza.jpeg"
+import Arslan from "./teamEmpImages/arslan.jpeg"
+import Usman from "./teamEmpImages/Usman.jpeg"
+import Jawad from "./teamEmpImages/Jawad.jpeg"
+import Wahab from "./teamEmpImages/Wahab.jpeg"
+// import TemporaryImage from "./teamEmpImages/temporaryImage.png"
 const empData = [
   {
-    empName: "Munsif",
+    empName: "Munsif Ali Msri",
     jobTitle: "Tech Lead",
     university: "NUML",
+    empImage: Munsif
   },
   {
-    empName: "Kazmii",
+    empName: "Hassan Ali Shah ",
     jobTitle: "Front End Engineer",
     university: "NUML",
+    empImage: Hassan
   },
   {
-    empName: "Hamza",
+    empName: "Hamza Zaman",
     jobTitle: "Product Manager",
     university: "NUML",
+    empImage: Hamza
   },
   {
-    empName: "Arslan",
-    jobTitle: "Netwrok Analyst",
+    empName: "Arslan Safdar",
+    jobTitle: "Netwrok Analyist",
     university: "NUML",
+    empImage: Arslan
   },
   {
     empName: "Abdul Wahab",
     jobTitle: "BackEnd Developer",
     university: "NUML",
+    empImage: Wahab
+  },
+  {
+    empName: "Jawad Malik",
+    jobTitle: "UI/UX Developer",
+    university: "NUML",
+    empImage: Jawad
   },
   {
     empName: "Usman Shams",
     jobTitle: "Android Developer",
     university: "NUML",
+    empImage: Usman
   },
 ];
 export default empData;
